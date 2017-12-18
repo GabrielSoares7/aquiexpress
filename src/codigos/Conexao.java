2edf6e00397ee6a7db25129ee204b91aa643e4b3
Gabriel Soares <Gabriel77soares77@gmail.com>
2017-12-18 15:37:37 -0200

    Comentarios

--- src/codigos/Conexao.java
+++ src/codigos/Conexao.java
@@ -5,33 +5,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-public class Conexao {
+public class Conexao {//nome da classe para fazer a integração com o banco
     
-    private Connection conexao;
+    private Connection conexao;//abaixo está as variáveis que seram utilizadas para a integração do banco
     private String url;
     private String usuario;
     private String senha;
     
     public Conexao() {
+        //trutor será utilzado para fazer a integração com o banco de dados
         conexao = null;
-        url = "jdbc:mysql://localhost/DB_ESTOQUE";
-        usuario = "root";
-        senha ="c74387";
-        try {
-            conexao = DriverManager.getConnection(url, usuario, senha);
+        url = "jdbc:mysql://localhost/DB_ESTOQUE";//Nome da base de dados
+        usuario = "root";//Nome do usuario padrão entretanto pode ser alterado de acordo com a vontade do usuario
+        senha ="c74387";//senha do usuario
+        try {//Estamos utilizando o try porque estamos vamos converter os dados inseridos aqui para ser transportado para o banco de dados
+            conexao = DriverManager.getConnection(url, usuario, senha);// ira abrir uma conexão com os dados passados acima
         }
-        catch(SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Desculpe!\nVocê está desconectado");  
-            ex.printStackTrace();
+        catch(SQLException ex) {//Se caso acontecer algum erro na integração do banco junto com o projeto será mostrado aqui
+            JOptionPane.showMessageDialog(null, "Desculpe!\nVocê está desconectado");//Mensagem de erro
+            ex.printStackTrace();//Vai mostrar exatamente onde está o erro, no caso a linha do erro
         }
     }
 
-    public Connection getConexao() {
-        return conexao;
+    public Connection getConexao() {// Método para retonar e mostar a conexão
+        return conexao;//retornar a conexao
     }
 
     public void setConexao(Connection conexao) {
-        this.conexao = conexao;
+        this.conexao = conexao;// esta referenciando a antiga variavel com o nome conexao com a nova variavel do tipo conexao
     }
     
     
