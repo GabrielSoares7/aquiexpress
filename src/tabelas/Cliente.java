339486990841d4eb1bb8521df373f9d5dab0da80
Gabriel Soares <Gabriel77soares77@gmail.com>
2017-12-18 16:44:54 -0200

    Comentarios

--- src/tabelas/Cliente.java
+++ src/tabelas/Cliente.java
@@ -4,7 +4,10 @@ import tabelas.Pessoa;
 
 public class Cliente extends Pessoa{
     private String nascimento;
-
+    /* Classe com o objetivo de passar as variáveis que estão 
+    *tanto no banco quanto no projeto.
+    *Aqui estão resentados, e  inseridos também construtores e métodos getters e setters
+    */
     public Cliente(int id, String nascimento, String nome,
             String login, String senha) {
         super(id, nome, login, senha);
